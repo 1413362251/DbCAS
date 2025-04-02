f55cf8a78bc1735073de3cbbf5090d5a16bd58ba
WestFang <1413362251@qq.com>
2025-04-02 10:54:55 +0100

    Save 9th Jul

--- Web/app.py
+++ Web/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request
 import sqlite3
 import os
 
@@ -15,13 +16,26 @@ def home():
 @app.route('/search')
 def search():
     # 连接数据库并获取数据
+    keyword = request.args.get('q', '').strip()
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM database_info")  # 替换 your_table_name 为你的实际表名
+    if keyword:
+        search_columns = [
+            'Name', 'Public_Date', 'Data Type', 'Data Format', 'Data Volume',
+            'Species', 'Tissues', 'Diseases', 'Source', 'DOI', 'Url', 'Citation',
+            'Description'
+        ]
+        where_clause = " OR ".join([f'"{col}" LIKE ?' for col in search_columns])
+        sql = f"SELECT * FROM database_info WHERE {where_clause}"
+        params = tuple(['%' + keyword + '%'] * len(search_columns))
+        cursor.execute(sql, params)
+    else:
+        cursor.execute("SELECT * FROM database_info")
+
     data = cursor.fetchall()
     conn.close()
 
-    return render_template('search.html', query=data)
+    return render_template('search.html', query=data,keyword=keyword)
 
 if __name__ == "__main__":
     app.run(debug=True)
